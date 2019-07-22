12a130eadf819ab16e6345093db7dd0511ea4577
phy5prt <philip.r.tate@gmail.com>
2019-07-22 18:00:52 +0100

    deleted files that wernt being used now that were running from app.js

--- frontEnd/Javascript/utils.js
+++
@@ -1,9 +0,0 @@
-/*jshint esversion: 6 */
-
-var windowHeight = $(window).height();
-
-
-
-
-//should keep const here that grab the marbles etc
-//probably need a page manager function for make files relevant and use that in the pg.js to call events
