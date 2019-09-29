aacda289f42c671a6eb1cd138edaab29b25906a3
phy5prt <philip.r.tate@gmail.com>
2019-09-29 17:19:56 +0100

    IT SPINS (the carousel)

--- public/Javascript/carousel.js
+++ public/Javascript/carousel.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 /*when refactor consider using .done rather than succes*/
 var saughtTechnologyProjectCards=[{}];
-
+var showcase = $("#showcase")
 function getSaughtTechnologyProjectCardsAndMakeCarousel(saughtTechnology) {
   console.log("using get saught technology");
 var sghtCrds;
@@ -169,11 +169,65 @@ $showcase.append(cloud9CardTemplate.clone(true));
 
 
 
-      showcase.Cloud9Carousel();
+
+
+showcase.Cloud9Carousel( {
+yOrigin: 150, /* Calc js and get half of the viewport height */
+
+ yRadius: 150,
+  farScale:0.5,
+  itemClass: "cloud9-item",
+  buttonLeft: $(".nav.left"),
+  buttonRight: $(".nav.right"),
+  buttonBottom:$(".nav.bottom"),
+  buttonTop:$('.nav.top'), /*but there isnt one*/
+  bringToFront: true,
+  onLoaded: function() {
+    showcase.css( 'visibility', 'visible' )
+    showcase.css( 'display', 'none' )
+    showcase.fadeIn( 100 )
+  }
+} )
+
+//
+// Simulate physical button click effect
+//
+
+$('.nav').click( function( e ) {
+  var b =
+
+  $(e.target).addClass( 'down' )
+  setTimeout( function() { b.removeClass( 'down' ) }, 80 )
+} )
+
+$(document).keydown( function( e ) {
+  //
+  // More codes: http://www.javascripter.net/faq/keycodes.htm
+  //
+  switch( e.keyCode ) {
+    /* left arrow */
+    case 37:
+      $('.nav.left').click()
+      break
+      /* I added down/bottom arrow */
+      case 40:
+        $('.nav.bottom').click()
+        break
+/* I added up shall be to open it arrow */
+        case 38:
+          $('.nav.top').click()
+          break
+
+    /* right arrow */
+    case 39:
+      $('.nav.right').click()
+  }
+} )
+}
 
 
 // }
-}
+
 
 
 $(".aMarble").click(
--- public/Javascript/dummyData.js
+++ public/Javascript/dummyData.js
@@ -1,62 +1,62 @@
-// var projectCards = [{
-//   projectName:" Project Title ",
-//   overallProjectRating:"10",
-//   projectDescription: "A very very good project",
-//   projectImagePath:" Images/postman.jpg ",
-//   technologiesArray:[{
-//     technologyName:"unity",
-//     technologyImagePath:" Images/tea.png ",
-//     technologyExampleRating:"10"
-//   },
-//   {
-//     technologyName:"unity",
-//     technologyImagePath:" Images/tea.png ",
-//     technologyExampleRating:"10"
-//   },
-//   {
-//     technologyName:"unity",
-//     technologyImagePath:" Images/postman.jpg ",
-//     technologyExampleRating:"10"
-//   }],
-//   linksArray:[{
-//     linkName:"unity",
-//     linkImagePath:" Images/postman.jpg ",
-//     linkHyperlink:"https://unity.com/ "
-//   },
-//   {
-//     linkName:"unity",
-//     linkImagePath:" Images/Shotglass.png ",
-//     linkHyperlink:"https://unity.com/ "
-//   },
-//   {
-//     linkName:"unity",
-//     linkImagePath: " Images/cocktailStraw.png ",
-//     linkHyperlink:"https://unity.com/ "
-//   }]
-// }, {
-//   projectName: " Example2  ",
-//   overallProjectRating:"10",
-//   projectDescription:" Lauren Ipsum Latin bloke some writting here as vague as smoke, all it gives is space and shape, with less draw backs than a vape",
-//   projectImagePath: " Images/cocktailOlive.png " ,
-//   technologiesArray:[
-//   {
-//     technologyName:"batman",
-//     technologyImagePath:"  Images/cocktailOlive.png  ",
-//     technologyExampleRating:"10"
-//   },
-//   {
-//     technologyName:"unity",
-//     technologyImagePath:" Images/postman.jpg ",
-//     technologyExampleRating:"10"
-//   }],
-//   linksArray:[{
-//     linkName:"unity",
-//     linkImagePath:" Images/postman.jpg ",
-//     linkHyperlink:" https://unity.com/ "
-//   },
-//   {
-//     linkName:"unity",
-//     linkImagePath:" Images/waterglass.png ",
-//     linkHyperlink:" https://unity.com/ "
-//   }]
-// }]
+var projectCards = [{
+  projectName:" Project Title ",
+  overallProjectRating:"10",
+  projectDescription: "A very very good project",
+  projectImagePath:" Images/postman.jpg ",
+  technologiesArray:[{
+    technologyName:"unity",
+    technologyImagePath:" Images/tea.png ",
+    technologyExampleRating:"10"
+  },
+  {
+    technologyName:"unity",
+    technologyImagePath:" Images/tea.png ",
+    technologyExampleRating:"10"
+  },
+  {
+    technologyName:"unity",
+    technologyImagePath:" Images/postman.jpg ",
+    technologyExampleRating:"10"
+  }],
+  linksArray:[{
+    linkName:"unity",
+    linkImagePath:" Images/postman.jpg ",
+    linkHyperlink:"https://unity.com/ "
+  },
+  {
+    linkName:"unity",
+    linkImagePath:" Images/Shotglass.png ",
+    linkHyperlink:"https://unity.com/ "
+  },
+  {
+    linkName:"unity",
+    linkImagePath: " Images/cocktailStraw.png ",
+    linkHyperlink:"https://unity.com/ "
+  }]
+}, {
+  projectName: " Example2  ",
+  overallProjectRating:"10",
+  projectDescription:" Lauren Ipsum Latin bloke some writting here as vague as smoke, all it gives is space and shape, with less draw backs than a vape",
+  projectImagePath: " Images/cocktailOlive.png " ,
+  technologiesArray:[
+  {
+    technologyName:"batman",
+    technologyImagePath:"  Images/cocktailOlive.png  ",
+    technologyExampleRating:"10"
+  },
+  {
+    technologyName:"unity",
+    technologyImagePath:" Images/postman.jpg ",
+    technologyExampleRating:"10"
+  }],
+  linksArray:[{
+    linkName:"unity",
+    linkImagePath:" Images/postman.jpg ",
+    linkHyperlink:" https://unity.com/ "
+  },
+  {
+    linkName:"unity",
+    linkImagePath:" Images/waterglass.png ",
+    linkHyperlink:" https://unity.com/ "
+  }]
+}]
