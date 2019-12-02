9fc966fa22e73aa56b438819eea62c1943f5c8e2
phy5prt <philip.r.tate@gmail.com>
2019-12-02 15:21:37 +0000

    website redirects for small devices

--- public/Javascript/utils.js
+++ public/Javascript/utils.js
@@ -1,5 +1,14 @@
 /*jshint esversion: 6 */
 
+$(document).ready(function(){
+  if($(window).width() <= 950) {
+    alert("redirecting because my creative portfolio site isn't optimized yet for smaller devices, but my bootstrap one it. The content is the same. You can see youtube of my creatie site on my channel if you wish.");
+window.location = "https://phy5prt.github.io/BootstrapPortfolioWebsite/";
+}
+});
+
+
+
 var windowHeight = $(window).height();
 
 function anchorClick(){
